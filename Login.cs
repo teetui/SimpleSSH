6c1fb7c5e4688b83504f6fc5b6d33f05cbf03261
Teetui <samfoit@gmail.com>
2021-04-17 21:17:46 -0400

    Add project files.

---
+++ Login.cs
@@ -0,0 +1,79 @@
+﻿using Renci.SshNet;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace SimpleSSH
+{
+    class Login
+    {
+        public string Host = string.Empty;
+        public string UserID = string.Empty;
+        public string Port = "22";
+        public string Password = string.Empty;
+        private bool HasKeyFile = false;
+        private SshClient client;
+
+        public void SetKeyFile(StorageFile keyFile)
+        {
+            Windows.Storage.AccessCache.StorageApplicationPermissions.
+                FutureAccessList.AddOrReplace("PickedFileToken", keyFile);
+            HasKeyFile = true;
+        }
+
+        public async Task<StorageFile> GetKeyFile()
+        {
+            StorageFile keyFile = await Windows.Storage.AccessCache.StorageApplicationPermissions.
+                FutureAccessList.GetFileAsync("PickedFileToken");
+
+            return keyFile;
+        }
+
+        public bool Connect()
+        {
+            if (Host == string.Empty || UserID == string.Empty)
+                return false;
+            if (Password == string.Empty && HasKeyFile == false)
+                return false;
+
+            try
+            {
+                ConnectionInfo connectionInfo;
+                if (HasKeyFile)
+                    connectionInfo = new ConnectionInfo(Host, UserID, 
+                        new PrivateKeyAuthenticationMethod(UserID, new PrivateKeyFile(GetKeyFile().Result.Path)));
+                else
+                    connectionInfo = new ConnectionInfo(Host, UserID,
+                        new PasswordAuthenticationMethod(UserID, Password));
+
+                client = new SshClient(connectionInfo);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+
+            return true;
+        }
+
+        public string Command(string command)
+        {
+            SshCommand result;
+            if (Connect())
+            {
+                client.Connect();
+                result = client.RunCommand(command);
+                client.Disconnect();
+            }
+            else
+                return "Connection failed.";
+
+            return result.Result;
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Extensions.Logging.Abstractions;
+using Renci.SshNet;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace SimpleSSH
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private readonly Login login = new Login();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void ConnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            login.Host = HostTextBox.Text;
+            login.UserID = UserIDTextBox.Text;
+            login.Password = PasswordBox.Password;
+
+            string result;
+            if (CommandTextBox.Text != string.Empty)
+                result = login.Command(CommandTextBox.Text);
+            else
+                result = "No command specified.";
+
+            // Create the message dialog and set its content
+            var messageDialog = new MessageDialog(result);
+            await messageDialog.ShowAsync();
+        }
+
+        private async void KeyFileBrowseButton_Click(object sender, RoutedEventArgs e)
+        {
+            FileOpenPicker openPicker = new FileOpenPicker();
+            openPicker.ViewMode = PickerViewMode.Thumbnail;
+            openPicker.FileTypeFilter.Add(".ppk");
+
+            StorageFile keyFile = await openPicker.PickSingleFileAsync();
+            if (keyFile != null)
+            {
+                // Application now has read/write access to the picked file
+                KeyTextBox.Text = keyFile.Path;
+
+                // copy keyfile to local folder,
+                // because UWP file access permissions
+                // don't seem to work in SSH.Net
+                StorageFile newKeyFile = await keyFile.CopyAsync(ApplicationData.Current.LocalFolder, 
+                    keyFile.Name, NameCollisionOption.ReplaceExisting);
+                login.SetKeyFile(newKeyFile);
+            }
+        }
+    }
+}
